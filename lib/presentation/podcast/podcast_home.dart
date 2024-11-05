51de6e77289a751a0ddc7f7c14e412c293e7a582
bruno <1201ba@gmail.com>
2024-11-05 18:15:26 +0100

    Starting with podcast feature

---
+++ lib/presentation/podcast/podcast_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PodcastHome extends StatelessWidget {
+  const PodcastHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
