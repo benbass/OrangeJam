c170ff62bef760c2c54288b25b460ac4c069003b
bruno <1201ba@gmail.com>
2025-07-19 09:51:07 +0200

    Wrapped BottomSheetPlaylistsMenu with SafeArea

--- lib/presentation/homepage/playlists_menu/bottom_sheet_playlists_menu.dart
+++ lib/presentation/homepage/playlists_menu/bottom_sheet_playlists_menu.dart
@@ -15,38 +15,40 @@ class BottomSheetPlaylistsMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/orange.jpeg"),
-          fit: BoxFit.fitWidth,
-          opacity: 0.35,
-          alignment: Alignment.center,
-          colorFilter: ColorFilter.mode(
-            Color(0xFFFF8100),
-            BlendMode.darken,
-          ),
-        ),
-        color: Color(0xFFFF8100),
-      ),
-      child: Wrap(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 6),
-            child: Column(
-              children: [
-                const CreatePlaylistButton(),
-                FirstRowAllFilesAndQueueButtons(
-                  scrollController: scrollController,
-                ),
-                const SizedBox(height: 15,),
-                SecondRowPlaylistButtons(
-                  scrollController: scrollController,
-                ),
-              ],
+    return SafeArea(
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/orange.jpeg"),
+            fit: BoxFit.fitWidth,
+            opacity: 0.35,
+            alignment: Alignment.center,
+            colorFilter: ColorFilter.mode(
+              Color(0xFFFF8100),
+              BlendMode.darken,
             ),
-          )
-        ],
+          ),
+          color: Color(0xFFFF8100),
+        ),
+        child: Wrap(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 6),
+              child: Column(
+                children: [
+                  const CreatePlaylistButton(),
+                  FirstRowAllFilesAndQueueButtons(
+                    scrollController: scrollController,
+                  ),
+                  const SizedBox(height: 15,),
+                  SecondRowPlaylistButtons(
+                    scrollController: scrollController,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
